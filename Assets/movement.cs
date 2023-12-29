2b0e20608137be339d11465c66e1b0764034c99b
adityakedia006 <b22ee006@iitj.ac.in>
2023-12-29 18:44:41 +0530

    controls

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Management;
 
 public class movement : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 5f;
+    public float backwardDistance = 1f;
+    private bool isJumping = false;
 
     // Update is called once per frame
     void Update()
@@ -14,5 +16,11 @@ public class movement : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
         transform.Translate(movement);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+        }
     }
 }
